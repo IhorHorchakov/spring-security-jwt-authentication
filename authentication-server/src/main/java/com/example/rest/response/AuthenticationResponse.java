41272a12eb89f92e77d6dcab8de8ed94433b72a5
Igor.Gorchakov <igor.gorchakov@easypark.net>
2023-10-29 23:31:36 +0200

    Updated README.md

---
+++ authentication-server/src/main/java/com/example/rest/response/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.example.rest.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String accessJwt;
+    private String refreshJwt;
+}
